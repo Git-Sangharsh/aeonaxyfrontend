b861c81f0553462ae13047df54a50793dc9c7be9
sangharsh <sotumotghare@gmail.com>
2024-04-03 04:22:15 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React from 'react'
-import './App.css'
-import Signup from './components/signup/Signup'
+import React from "react";
+import "./App.css";
+import Signup from "./components/signup/Signup";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Profile from "./components/profile/Profile";
 
 const App = () => {
   return (
-    <div className='App'>
-      <Signup />
-    </div>
-  )
-}
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Signup />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 
-export default App
+export default App;
--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,8 +1,26 @@
 import React from 'react'
-
+import './Profile.css'
+import dribbleLogo from '../assets/dribble-logo.png';
 const Profile = () => {
   return (
-    <div>Profile</div>
+    <div className='profile-container'>
+      <img className='dribble-logo' src={dribbleLogo} alt="" />
+      <div className="profile-wrapper">
+        <div className="profile-wrapper-inner">
+          <h1 className='profile-wrapper-header'>Welcome! Let's create your profile</h1>
+          <h1 className='profile-wrapper-header-2'>Let others get to know you better! you can do these later</h1>
+          <h1 className='profile-wrapper-header-3'>Add an avatar</h1>
+          <div className="avatar-div">
+            <div className='circle'> + </div>
+            <div className="avtar-choose-image">
+              <h3 className='choose-image' >Choose Image</h3>
+            <h1 className='profile-wrapper-header-2 defult-text'>Or choose one of our defaults</h1>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
